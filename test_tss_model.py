c208c2f0d60db05e4a25502fafae45bee23d9a74
Kaiyue Zhang <80300152+ylyzz21@users.noreply.github.com>
2023-02-14 09:40:34 +0800

    Add files via upload

---
+++ test_tss_model.py
@@ -0,0 +1,112 @@
+from mpn_models.dmpnn import MPNN
+from mpn_models import utils
+import os
+import argparse
+import torch
+from mpn_models import mpnn
+import ray
+from tqdm import tqdm
+import numpy as np
+from joblib import load
+import math
+import scipy.stats as stats
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+current_time=datetime.now()
+suffix=str(current_time.year)+"_"+str(current_time.month)+"_"+str(current_time.day)
+
+parser = argparse.ArgumentParser(description='Testing a target-specific scoring model.')
+parser.add_argument('model_path', type=str, 
+                    help='a path to a trained scoring model in ckpt format. It could be absolute path or relative path. Example: ./task_name/lightning_logs/version_0/checkpoints/epoch=4-step=79.ckpt')
+parser.add_argument('testing_dataset_path', type=str,
+                    help='a path to a file in csv format. It could be absolute path or relative path. Example: ./data/ampc_round2_test1k.csv The first column should be SMILES and second column should be score.')
+parser.add_argument('--task_name', type=str, default="data",required=False,
+                    help='name of the output folder located under swit/. default : data')
+parser.add_argument('--ncpu', type=int, default=1,required=False,
+                    help='the number of cores to available to each worker / job / process / node. default : 1')
+args = parser.parse_args()
+if not os.path.exists("./"+args.task_name+"/preds"):
+    cmd="mkdir -p ./"+args.task_name+"/preds"
+    os.system(cmd)
+current_work_dir = os.getcwd()
+os.chdir(current_work_dir)
+### load data
+scores_csv       = args.testing_dataset_path
+scores, failures = utils._read_scores(scores_csv)
+xs, ys           = zip(*scores.items())
+print(f"input data size:{len(xs)}")
+
+### load ckpt
+dict_pt = torch.load(args.model_path)["state_dict"]
+new_dict = {}
+for k in dict_pt.keys():
+    ki = k.split("mpnn.")[1]
+    new_dict[ki] = dict_pt[k]
+    
+### load model
+tss_model = MPNN()
+tss_model.model.load_state_dict(new_dict)
+tss_model.model.eval() 
+
+### Generate predictions for the inputs xs
+_predict = ray.remote(num_cpus=args.ncpu, num_gpus=1)(mpnn.predict)  
+use_gpu = ray.cluster_resources().get('GPU', 0) > 0
+smis_batches = utils.batches(xs, 10000)#00
+model = ray.put(tss_model.model)
+scaler_path=args.model_path.split("checkpoints/epoch=")[0]+"std_scaler.bin"
+scaler = ray.put(load(scaler_path))
+batch_size=50
+refs = [
+    _predict.remote(
+        model, smis, batch_size, args.ncpu,
+        True, scaler, use_gpu, True
+    ) for smis in smis_batches
+]
+preds_chunks = [
+    ray.get(r) for r in tqdm(refs, desc='Prediction', leave=False)
+]
+pred_scores=[]
+for idx in range(len(preds_chunks)):
+    for pred_score in preds_chunks[idx][0]:
+        pred_scores.append(pred_score[0])
+print(f"output data size:{len(pred_scores)}")
+
+### calculate RMSE,PCC between predicted score and target score
+if len(pred_scores)==len(ys):
+    se_lst = []
+    writer=open("./"+args.task_name+"/preds/prediction"+suffix+".csv","w")
+    writer.write("prediction,target\n")
+    new_pred=[]
+    new_ys=[]
+    for idx,target_score in enumerate(ys):
+        if target_score>=0:  ## filter that should not be the items
+            continue
+        se = (target_score-pred_scores[idx])**2
+        se_lst.append(se)
+        new_pred.append(pred_scores[idx])
+        new_ys.append(target_score)
+        writer.write(str(round(pred_scores[idx],1))+","+str(target_score)+"\n")
+    writer.close()
+    mse = np.average(se_lst)
+    rmse = math.sqrt(mse)
+    PCC,p =stats.pearsonr(new_pred,new_ys)
+    print(f"length of score list:{len(se_lst)}")
+    print(f"RMSE:{rmse:.2f}")
+    print(f"PCC :{PCC:.2f}")
+    ## plot
+    fig=plt.figure(figsize=(15,15))
+    x = np.linspace(-19,-1,19)
+    y = x
+    plt.scatter(pred_scores,ys,color='orange',s=6)
+    plt.xlim(-20,0)
+    plt.ylim(-20,0)
+    plt.tick_params(labelsize=20)
+    plt.plot(x, y,"b--")
+    plt.xlabel("prediction",size=30)
+    plt.ylabel("target",size=30)
+    plt.savefig("./"+args.task_name+"/preds/pred_target_scatter.png")
+else:
+    print("Warning: the length of the prediciton and target are not the same.")
+
+
---
+++ train_tss_model.py
@@ -0,0 +1,99 @@
+from mpn_models.dmpnn import MPNN
+from mpn_models.chemprop import *
+from mpn_models import utils
+import os
+import argparse
+import sys
+from joblib import dump
+import numpy as np
+import math
+import scipy.stats as stats
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+current_time=datetime.now()
+suffix=str(current_time.year)+"_"+str(current_time.month)+"_"+str(current_time.day)
+
+parser = argparse.ArgumentParser(description='Train a target-specific scoring model.')
+parser.add_argument('training_dataset_path', type=str, 
+                    help='a path to a file in csv format. It could be absolute path or relative path. Example: ./data/ampc_round2_test1k.csv The first column should be SMILES and second column should be score.')
+parser.add_argument('task_name', type=str, default=None,
+                    help='the name of the output folder located under swit/')
+parser.add_argument('--testing_dataset_path', type=str, default=None,
+                    help='a path to a file in csv format. It could be absolute path or relative path. Example: ./data/ampc_round2_test1k.csv The first column should be SMILES and second column should be score. default: None')
+parser.add_argument('--ncpu', type=int, default=1,
+                    help='number of cores to available to each worker/job/process/node. default: 1')
+parser.add_argument('--epochs', type=int, default=50,
+                    help='number of iterations for model training. default: 50')
+args = parser.parse_args()
+
+if not os.path.exists("./"+args.task_name+"/preds"):
+    cmd="mkdir -p ./"+args.task_name+"/preds"
+    os.system(cmd)
+current_work_dir = os.getcwd()
+os.chdir(current_work_dir)
+scores_csv       = args.training_dataset_path
+scores, failures = utils._read_scores(scores_csv)
+xs, ys           = zip(*scores.items())
+print(f"input data size:{len(xs)}")
+
+my_model= MPNN(ncpu=args.ncpu,epochs=args.epochs)
+my_model.train(xs, ys,args.task_name)
+#print(my_model)
+print("Trained model saved in /swit/"+args.task_name+"...")
+folder_name=sorted(os.listdir("./"+args.task_name+"/lightning_logs"))[-1]
+dump(my_model.scaler,f'./'+args.task_name+'/lightning_logs/'+folder_name+'/std_scaler.bin',compress=True)
+print("The scaler is already stored in {}".format('./'+args.task_name+'/lightning_logs/'+folder_name+'/std_scaler.bin'))
+
+### prediction
+if not args.testing_dataset_path is None:
+    ### load data
+    scores_csv       = args.testing_dataset_path
+    scores, failures = utils._read_scores(scores_csv)
+    xs, ys           = zip(*scores.items())
+    print(f"input data size:{len(xs)}")
+    preds_chunks=my_model.predict(xs)
+    pred_scores=[]
+    print(preds_chunks)
+    for idx in range(len(preds_chunks)):
+        for pred_score in preds_chunks[idx]:
+                pred_scores.append(pred_score[0])
+    print(f"output data size:{len(pred_scores)}")
+
+    ### calculate RMSE,PCC between predicted score and target score
+    if len(pred_scores)==len(ys):
+        se_lst = []
+        new_pred=[]
+        new_ys=[]
+        writer=open("./"+args.task_name+"/preds/prediction"+suffix+".csv","w")
+        writer.write("prediction,target\n")
+        for idx,target_score in enumerate(ys):
+            if target_score>=0:  ## filter that should not be the items
+                continue
+            se = (target_score-pred_scores[idx])**2
+            se_lst.append(se)
+            new_pred.append(pred_scores[idx])
+            new_ys.append(target_score)
+            writer.write(str(round(pred_scores[idx],1))+","+str(target_score)+"\n")
+        writer.close()
+        mse = np.average(se_lst)
+        rmse = math.sqrt(mse)
+        PCC,p =stats.pearsonr(new_pred,new_ys)
+        print(f"length of score list:{len(se_lst)}")
+        print(f"RMSE:{rmse:.2f}")
+        print(f"PCC :{PCC:.2f}")
+        ## plot
+        fig=plt.figure(figsize=(15,15))
+        x = np.linspace(-19,-1,19)
+        y = x
+        plt.scatter(pred_scores,ys,color='orange',s=6)
+        plt.xlim(-20,0)
+        plt.ylim(-20,0)
+        plt.tick_params(labelsize=20)
+        plt.plot(x, y,"b--")
+        plt.xlabel("prediction",size=30)
+        plt.ylabel("target",size=30)
+        plt.savefig("./"+args.task_name+"/preds/pred_target_scatter.png")
+    else:
+        print("Warning: the length of the prediciton and target are not the same.")
