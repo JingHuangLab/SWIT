638e9c1c3fd4e79b42ad005925eade0703ebcb53
Kaiyue Zhang <80300152+ylyzz21@users.noreply.github.com>
2023-02-14 11:23:27 +0800

    Update run_sbatch.sh

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -2,14 +2,14 @@
 #SBATCH -J swit_train
 #SBATCH -N 1
 #SBATCH -c 6
-#SBATCH -p A40
+#SBATCH -p your_card_name
 #SBATCH --gres=gpu:1
-#SBATCH -o /home/zhangky/rein_joblog/pred_%j.log
-#SBATCH -e /home/zhangky/rein_joblog/pred_%j.err
+#SBATCH -o /your/path/of/log/pred_%j.log
+#SBATCH -e /your/path/of/log/pred_%j.err
  
 module load mathlib/cuda/10.1.168_418.67
-source /home/zhangky/miniconda3/bin/activate /home/zhangky/miniconda3/envs/molpal
+source /your/path/of/conda/bin/activate /your/path/of/envs/env_name
 
-cd /home/zhangky/tool/swit/models
+cd /your/path/of/swit/models
 #python train_tss_model.py ../data/ampc_trainset.csv ../data/ampc_testset.csv ampc_model1 --ncpu 6 
-python test_tss_model.py ../ampc_model1/lightning_logs/version_5433864/checkpoints/epoch=40-step=433697.ckpt ../data/ampc_gen1_test.csv --task_name ampc_model1 --ncpu 6
+python test_tss_model.py ../task-name/ightning_logs/version_x/checkpoints/xxx.ckpt ../data/ampc_testset521.csv --task_name your_task_name --ncpu 6
