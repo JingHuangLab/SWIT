e861ddc27557f78e0fd56bc0403b6599cefafff1
KaiyueZhang <80300152+ylyzz21@users.noreply.github.com>
2023-02-16 12:02:20 +0800

    Update run_sbatch.sh

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -10,6 +10,9 @@
 module load mathlib/cuda/10.1.168_418.67
 source /your/path/of/conda/bin/activate /your/path/of/envs/env_name
 
-cd /your/path/of/swit/models
-#python train_tss_model.py ../data/ampc_trainset.csv ../data/ampc_testset.csv ampc_model1 --ncpu 6 
-python test_tss_model.py ../task-name/ightning_logs/version_x/checkpoints/xxx.ckpt ../data/ampc_testset521.csv --task_name your_task_name --ncpu 6
+cd /your/path/of/swit/
+#python train_tss_model.py data/ampc_trainset_demo.csv task_test --testing_dataset_path data/ampc_testset_demo.csv --ncpu 6 
+## 1. Please change to your own ckpt path
+## 2. Please also change the test set to the data path you want to use
+## 3. Please change the task name used during training
+python test_tss_model.py examples/task_test/lightning_logs/version_5434400/checkpoints/epoch=49-step=799.ckpt data/ampc_testset_demo.csv --task_name task_test --ncpu 6
